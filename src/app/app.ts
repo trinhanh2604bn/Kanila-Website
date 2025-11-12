81214d81317dd9cb7790f213ba7cb77a3ae8c8c3
phunm123123 <phunm23411@st.uel.edu.vn>
2025-11-12 19:55:28 +0700

    WIP: Lưu thay đổi cục bộ trước khi pull main

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // 1. SỬA TÊN IMPORT: Từ Header thành HeaderComponent
-// SỬA: Thêm .component vào cuối đường dẫn
-import { Header } from './core/header/header';
+import { Header} from './core/header/header';  // Sửa đường dẫn
+import { SidebarMenu} from './shared/sidebar-menu/sidebar-menu';
+
 @Component({
   // 2. SỬA SELECTOR: Chỉ giữ lại app-root
-  selector: 'app-root', 
+  selector: 'app-root',
   // 3. THÊM HeaderComponent VÀO IMPORTS
-  imports: [RouterOutlet, Header], 
+  imports: [RouterOutlet, Header, SidebarMenu],  // Thêm HeaderComponent vào đây
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'] // Sửa styleUrl thành styleUrls (vì styleUrls là mảng)
 })
 export class App {
   protected readonly title = signal('Kanila');
---
+++ src/app/shared/sidebar-menu/sidebar-menu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-menu',
+  imports: [],
+  templateUrl: './sidebar-menu.html',
+  styleUrl: './sidebar-menu.css',
+})
+export class SidebarMenu {
+
+}
