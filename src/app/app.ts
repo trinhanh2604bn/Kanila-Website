289e2f967a090596af0e22825b9d06a4f5c5719e
phunm123123 <phunm23411@st.uel.edu.vn>
2025-11-12 16:43:11 +0700

    Cập nhật component header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+// 1. SỬA TÊN IMPORT: Từ Header thành HeaderComponent
+// SỬA: Thêm .component vào cuối đường dẫn
+import { Header } from './core/header/header';
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  // 2. SỬA SELECTOR: Chỉ giữ lại app-root
+  selector: 'app-root', 
+  // 3. THÊM HeaderComponent VÀO IMPORTS
+  imports: [RouterOutlet, Header], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
