63e71ac4b9272deac10455e20323210b5afdc52e
GiaNganHelus <ngangia137@gmail.com>
2025-11-13 08:17:25 +0700

    Restore modules and layout from branch ThanhTruc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout, 
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/account-module/pages/profile-user/profile-user').then(m => m.ProfileUser)
+      }
+    ]
+  },
+
+  { path: '**', redirectTo: '' } // fallback
+];
+
--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Footer } from '../../core/footer/footer';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [Footer, RouterOutlet],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
