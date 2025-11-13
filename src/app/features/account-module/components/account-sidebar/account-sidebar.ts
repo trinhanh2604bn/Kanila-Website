13b493f1d1b736fee4e62938ddb39885713e5a78
ThanhTruc <trucltt23411@st.uel.edu.vn>
2025-11-13 16:03:10 +0700

    sidebar account

--- src/app/features/account-module/components/account-sidebar/account-sidebar.ts
+++ src/app/features/account-module/components/account-sidebar/account-sidebar.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CollapsePanel } from '../../../../shared/components/collapse-panel/collapse-panel';
 
 @Component({
   selector: 'app-account-sidebar',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, CollapsePanel],
   templateUrl: './account-sidebar.html',
   styleUrl: './account-sidebar.css',
 })
