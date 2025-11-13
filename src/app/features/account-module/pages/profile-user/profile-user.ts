f4cfda99f6e75d7018ba19bbd5279ff8565fdaf5
ThanhTruc <trucltt23411@st.uel.edu.vn>
2025-11-13 23:15:18 +0700

    tạo trang hồ sơ

--- src/app/features/account-module/pages/profile-user/profile-user.ts
+++ src/app/features/account-module/pages/profile-user/profile-user.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile-user',
+  standalone: true,
   imports: [],
   templateUrl: './profile-user.html',
   styleUrl: './profile-user.css',
