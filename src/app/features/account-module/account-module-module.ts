e6e302d034029f3733263e36ab737b3119ec268e
ThanhTruc <trucltt23411@st.uel.edu.vn>
2025-11-14 00:20:17 +0700

    Trang Password

--- src/app/features/account-module/account-module-module.ts
+++ src/app/features/account-module/account-module-module.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AccountShell } from './pages/account-shell/account-shell';
 import { ProfileUser } from './pages/profile-user/profile-user';
+import { ChangePassword } from './pages/change-password/change-password';
 
 
 export const AccountModuleModule: Routes = [
@@ -9,6 +10,7 @@ export const AccountModuleModule: Routes = [
     component: AccountShell,
     children: [
       {path: 'info', component: ProfileUser},
+      {path: 'password', component: ChangePassword},
 
      { path: '', redirectTo: 'info', pathMatch: 'full' }
     ]
