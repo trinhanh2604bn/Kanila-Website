528f3947d723ce41caa3abdef4e7d14f4b3f6380
ThanhTruc <trucltt23411@st.uel.edu.vn>
2025-11-15 01:19:53 +0700

    thêm trạng thái đơn hàng, sửa giao diện orderitem trong account

--- src/app/features/account-module/pages/order-management/orders-processing/orders-processing.ts
+++ src/app/features/account-module/pages/order-management/orders-processing/orders-processing.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrderCard, OrderItemData } from '../../../components/order-card/order-card';
+
 
 @Component({
+  standalone: true,
   selector: 'app-orders-processing',
-  imports: [],
+  imports: [CommonModule, OrderCard],
   templateUrl: './orders-processing.html',
-  styleUrl: './orders-processing.css',
+  styleUrls: ['../orders-status.css']
 })
 export class OrdersProcessing {
-
+  orders: { total: number; items: OrderItemData[] }[] = [
+    {
+      total: 600000,
+      items: [
+        {
+          image: 'assets/sample-product.png',
+          brand: 'Tên Thương Hiệu',
+          name: 'Tên sản phẩm',
+          quantity: 1,
+          price: 200000
+        },
+        {
+          image: 'assets/sample-product.png',
+          brand: 'Tên Thương Hiệu',
+          name: 'Tên sản phẩm',
+          quantity: 1,
+          price: 200000
+        },
+        {
+          image: 'assets/sample-product.png',
+          brand: 'Tên Thương Hiệu',
+          name: 'Tên sản phẩm',
+          quantity: 1,
+          price: 200000
+        }
+      ]
+    }
+  ];
 }
