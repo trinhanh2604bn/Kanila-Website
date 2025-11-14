07ac7cffba5b2103b291cc65dfec4ec286ed559a
ThanhTruc <trucltt23411@st.uel.edu.vn>
2025-11-14 13:10:34 +0700

    sửa vị trí header mainlayout

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Footer } from './footer/footer';
 import { RouterOutlet } from '@angular/router';
-import { Header } from '../header/header';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-main-layout',
