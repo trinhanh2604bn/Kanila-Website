10ed99c9664c6936a19de2205b53b22f234d26dd
ThanhTruc <trucltt23411@st.uel.edu.vn>
2025-11-14 17:27:12 +0700

    tạo khung chuyển trang trong ordermanagement

--- src/app/features/account-module/pages/order-management/order-management-shell/order-management-shell.ts
+++ src/app/features/account-module/pages/order-management/order-management-shell/order-management-shell.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-order-management-shell',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './order-management-shell.html',
   styleUrl: './order-management-shell.css',
 })
