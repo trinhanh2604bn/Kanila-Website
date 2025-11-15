642d0987176d5ff590c422b81348edfdf0ad7740
ThanhTruc <trucltt23411@st.uel.edu.vn>
2025-11-15 20:50:53 +0700

    pull header từ Phuminh

--- src/app/layouts/main-layout/header/header.spec.ts
+++ src/app/layouts/main-layout/header/header.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Header } from './header';
 
-describe('Header', () => {
+describe('Footer', () => {
   let component: Header;
   let fixture: ComponentFixture<Header>;
 
