e4b49843dc2cd0411cbb45b2c837f2870ffbba4d
phunm123123 <phunm23411@st.uel.edu.vn>
2025-11-15 20:25:03 +0700

    Update header layout & styles

---
+++ src/app/layouts/main-layout/header/header.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './header.html',
+  styleUrls: ['./header.css'],
+})
+export class Header {
+  // trạng thái menu dropdown trên mobile
+  menuVisible = false;
+
+  toggleMenu() {
+    this.menuVisible = !this.menuVisible;
+  }
+
+  subscribe(ev: Event) {
+    ev.preventDefault();
+    alert('Cảm ơn bạn đã đăng ký!');
+  }
+}
